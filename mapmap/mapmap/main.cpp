a0cd88211c5f3d9d55e8357aee3fda198cd001af
leesj <tkdrmsdl80@naver.com>
2022-06-15 12:50:11 +0900

    010

---
+++ mapmap/mapmap/main.cpp
@@ -0,0 +1,46 @@
+// map 0.0
+
+#include <iostream>
+
+#include <string>
+#include <list>
+#include <vector>
+#include <map>
+
+
+using namespace std;
+
+
+
+int main(void)
+{
+	/*
+	map<string, int> Numbers;
+
+	//	   Key, Value
+	// map<int, int>
+
+	Numbers["A"] = 1;
+	Numbers["B"] = 2;
+	Numbers["C"] = 3;
+	Numbers["D"] = 4;
+	Numbers["E"] = 5;
+
+
+	for (map<string, int>::iterator iter = Numbers.begin();
+		iter != Numbers.end() ; ++iter)
+	{
+		cout << iter->first << endl;
+		cout << iter->second << endl << endl;
+	}
+	*/
+
+	enum { aaa, bbb, ccc, ddd, eee };
+
+	const int Max = 10;
+
+	int Array[Max];
+
+
+	return 0;
+}
